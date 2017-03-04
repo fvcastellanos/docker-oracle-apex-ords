424ad4b99b7fcc71640650d241745108658cf832
Jorge Alvarez <alvarez.jeap@gmail.com>
2017-03-03 23:49:47 -0600

    Add support for volumes

--- scripts/install_main.sh
+++ scripts/install_main.sh
@@ -5,40 +5,35 @@ exec 2>&1
 
 apt-get update && apt-get install -y unzip vim curl
 
-# Download files
-echo "--------------------------------------------------"
-echo "Downloading all files............................."
-./scripts/download_files.sh
-#
 #
 echo "--------------------------------------------------"
 echo "Installing SSH...................................."
-./scripts/install_ssh.sh
+./install_ssh.sh
 #
 #
 echo "--------------------------------------------------"
 echo "Installing JAVA..................................."
-./scripts/install_java.sh
+./install_java.sh
 #
 #
 echo "--------------------------------------------------"
 echo "Installing TOMCAT................................."
-./scripts/install_tomcat.sh
+./install_tomcat.sh
 #
 #
 echo "--------------------------------------------------"
 echo "Installing ORACLE XE.............................."
-./scripts/install_oracle.sh
+./install_oracle.sh
 #
 #
 echo "--------------------------------------------------"
 echo "Installing ORACLE APEX............................"
-./scripts/install_apex.sh
+./install_apex.sh
 #
 #
 echo "--------------------------------------------------"
 echo "Installing ORACLE APEX............................"
-./scripts/install_ords.sh
+./install_ords.sh
 #
 #
 echo "--------------------------------------------------"
